821c3ad09be93d2307e31d332694990a72c3f284
BeCare666 <becare.fr.ge@gmail.com>
2023-01-06 17:31:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//login fonction
+var inscriptoId = document.getElementById("inscriptoId");
+var connecto = document.getElementById("connecto");
+
+var connectoId = document.getElementById("connectoId");
+var inscripto = document.getElementById("inscripto");
+var exampleModalLabelid = document.getElementById("exampleModalCenterid");
+
+// function pour payer sur KKiapay
+var payerMa = document.querySelectorAll(".payerMaformationId");
+payerMa.forEach((p) => {
+  p.addEventListener("click", function () {
+    openKkiapayWidget({
+      amount: "10000",
+      position: "center",
+      callback: "javascript:sendmycommandinCentremodale()",
+      data: "",
+      theme: "blue",
+      key: "deb48fc468f8e7fcc35aee7ae721254a3427f5e5",
+    });
+    //la reponse du payement kkiapay
+    addSuccessListener((response) => {
+      console.log(response);
+      var trans = response.transactionId;
+      //  console.log(trans);
+      if (trans) {
+        localStorage.setItem("storageName", trans);
+        //window.location.href = "formation.html";
+        alert('Bravooooo! Votre inscription à été bien effectuée. BeCare vas vous contacter maintenant à fin de commencer la mise en place de votre business !')
+        
+      } else {
+        alert(`quelque chose s'est mal passé`);
+      }
+    });
+  });
+});
+
